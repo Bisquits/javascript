793eb9598992b194a60cbe422c66b697d23b9a43
Melinda Uivari <uivarimelinda23@gmail.com>
2023-07-25 23:58:59 +0300

    Solved exercise

--- reorder-tasks/index.js
+++ reorder-tasks/index.js
@@ -1,6 +1,39 @@
-
 function initialize() {
-   
+	var downButtons = document.querySelectorAll('.downButton');
+	var upButtons = document.querySelectorAll('.upButton');
+
+	downButtons.forEach(function (downButton) {
+		downButton.addEventListener('click', function () {
+			var currentContainer = this.parentNode;
+			var nextContainer = currentContainer.nextElementSibling;
+
+			if (nextContainer) {
+				var nextTask = nextContainer.querySelector('span');
+				var currentTask = currentContainer.querySelector('span');
+
+				currentContainer.replaceChild(nextTask, currentTask);
+				nextContainer.appendChild(currentTask);
+			}
+		});
+	});
+
+	upButtons.forEach(function (upButton) {
+		upButton.addEventListener('click', function () {
+			var currentContainer = this.parentNode;
+			var previousContainer = currentContainer.previousElementSibling;
+
+			if (previousContainer) {
+				var previousTask = previousContainer.querySelector('span');
+				var currentTask = currentContainer.querySelector('span');
+
+				currentContainer.replaceChild(previousTask, currentTask);
+				previousContainer.insertBefore(
+					currentTask,
+					previousContainer.lastElementChild
+				);
+			}
+		});
+	});
 }
 
 document.body.innerHTML = `
@@ -16,11 +49,11 @@ document.body.innerHTML = `
         <span>Monthly report</span>
         <button class="upButton" type="button">&uarr;</button>
     </div>`;
-  
+
 initialize();
 
-document.querySelectorAll("button")[0].click();
-document.querySelectorAll("button")[3].click();
-document.querySelectorAll("button")[1].click();
+document.querySelectorAll('button')[0].click();
+document.querySelectorAll('button')[3].click();
+document.querySelectorAll('button')[1].click();
 
 console.log(document.body.innerHTML);
